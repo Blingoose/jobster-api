6936ec417f557c75c990b59cf41f64c5864494f1
Andre Katov <andy.katov@icloud.com>
2022-12-29 18:11:44 +0200

    added pagination functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -52,8 +52,18 @@ export const getAllJobs = asyncWrapper(async (req, res, next) => {
     result = result.sort(sortCriteria[sort]);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result;
-  res.status(StatusCodes.OK).json({ jobs });
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 });
 
 export const getJob = asyncWrapper(async (req, res, next) => {
