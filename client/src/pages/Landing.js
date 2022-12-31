bb06ed405ba66c6fb42e7a61c3e573ae551684ad
Andre Katov <andy.katov@icloud.com>
2022-12-31 19:43:01 +0200

    edited landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,29 +1,29 @@
-import main from '../assets/images/main.svg';
-import Wrapper from '../assets/wrappers/LandingPage';
-import { Logo } from '../components';
-import { Link } from 'react-router-dom';
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
         <Logo />
       </nav>
-      <div className='container page'>
+      <div className="container page">
         {/* info */}
-        <div className='info'>
+        <div className="info">
           <h1>
             job <span>tracking</span> app
           </h1>
           <p>
-            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
-            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
-            crucifix heirloom meggings bicycle rights.
+            In times where we apply for many jobs in the pursuit of finding the
+            best match for ourselves, Jobster Tracking App will ensure that
+            you'll never forget what and where. Try it now!
           </p>
-          <Link to='/register' className='btn btn-hero'>
+          <Link to="/register" className="btn btn-hero">
             Login/Register
           </Link>
         </div>
-        <img src={main} alt='job hunt' className='img main-img' />
+        <img src={main} alt="job hunt" className="img main-img" />
       </div>
     </Wrapper>
   );
